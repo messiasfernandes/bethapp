71437f253b76809dd91f9d5c08aa6a653d5e589f
messiasfernandes <messiasfernandes@gmail.com>
2023-07-07 09:09:15 -0300

    ttt

--- src/app/service/notafiscal.service.ts
+++ src/app/service/notafiscal.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Servicemodel } from './servicemodel';
+import { Observable } from 'rxjs';
+import { Filtro } from '../model/filtro';
 
 @Injectable({
   providedIn: 'root'
 })
-export class NotafiscalService {
+export class NotafiscalService implements Servicemodel{
 
   constructor() { }
+  pesquisar(filtro: Filtro): Observable<any> {
+    throw new Error('Method not implemented.');
+  }
+  detalhar(id: number): Observable<any> {
+    throw new Error('Method not implemented.');
+  }
+  salvar(objeto: any): Observable<any> {
+    throw new Error('Method not implemented.');
+  }
+  editar(objeto: any): Observable<any> {
+    throw new Error('Method not implemented.');
+  }
+  excluir(id: number): Observable<any> {
+    throw new Error('Method not implemented.');
+  }
 }
