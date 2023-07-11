dd6f735f67f26330c164f1c141ddaf1996aaf662
messias fernandes <messiasfernandes@gmail.com>
2023-07-11 07:03:05 -0300

    implemetacao service Nota fiscal

--- src/app/notafiscal/consultarnotafiscal/consultarnotafiscal.component.ts
+++ src/app/notafiscal/consultarnotafiscal/consultarnotafiscal.component.ts
@@ -1,20 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LazyLoadEvent } from 'primeng/api';
+import { throwError } from 'rxjs/internal/observable/throwError';
+import { catchError } from 'rxjs/internal/operators/catchError';
 import { Filtro } from 'src/app/model/filtro';
+import { ErrohandlerService } from 'src/app/service/errohandler.service';
+import { NotafiscalService } from 'src/app/service/notafiscal.service';
 
 @Component({
   selector: 'app-consultarnotafiscal',
   templateUrl: './consultarnotafiscal.component.html',
   styleUrls: ['./consultarnotafiscal.component.css']
 })
-export class ConsultarnotafiscalComponent {
+export class ConsultarnotafiscalComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
   notafiscalfiltro = new Filtro()
   notasfiscais: any[] = [];
   totalRegistros = 0;
   url:string;
-  buscar(pagina: number = 0): void {
+  constructor(private notafiscalService : NotafiscalService,
+    private erroService: ErrohandlerService,){
 
   }
+  buscar(pagina: number = 0): void {
+    this.notafiscalfiltro.pagina = pagina;
+    this.notafiscalService
+      .pesquisar(this.notafiscalfiltro)
+      .pipe(
+        catchError((erro: any) => {
+          return throwError(() => this.erroService.erroHandler(erro));
+        })
+      )
+      .subscribe((dados: any) => {
+        console.log(dados.content);
+        this.notasfiscais = dados.content;
+
+        this.totalRegistros = dados.totalElements;
+      });
+  }
+
+
   aoMudarPagina(event: LazyLoadEvent) {
     const pagina = event!.first! / event!.rows!;
     this.buscar(pagina);
--- src/app/service/notafiscal.service.ts
+++ src/app/service/notafiscal.service.ts
@@ -2,15 +2,33 @@ import { Injectable } from '@angular/core';
 import { Servicemodel } from './servicemodel';
 import { Observable } from 'rxjs';
 import { Filtro } from '../model/filtro';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { config } from '../shared/config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotafiscalService implements Servicemodel{
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
   pesquisar(filtro: Filtro): Observable<any> {
-    throw new Error('Method not implemented.');
+    const headers = new HttpHeaders().append(
+      'Content-Type',
+      'application/json'
+    );
+    let params = new HttpParams()
+      .set('page', filtro.pagina.toString())
+      .set('size', filtro.itensPorPagina.toString());
+
+    if (filtro.parametro) {
+      params = params.set('paramentro', filtro.parametro);
+    }
+    const response = this.http.get<any>(`${config.baseurl}importarnotasfiscais`, {
+      headers,
+      params,
+    });
+
+    return response;
   }
   detalhar(id: number): Observable<any> {
     throw new Error('Method not implemented.');
