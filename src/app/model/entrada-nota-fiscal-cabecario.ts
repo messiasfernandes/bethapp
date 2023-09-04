d007b4daa6693616aca28b22ece40cd9a12d5e55
messias fernandes <messiasfernandes@gmail.com>
2023-09-03 23:19:04 -0300

    mudacanca entidad entrada nota fiscal

--- src/app/model/entrada-nota-fiscal-cabecario.ts
+++ src/app/model/entrada-nota-fiscal-cabecario.ts
@@ -6,8 +6,8 @@ import { TransporteNotafiscal } from './transporte-notafiscal';
 export class EntradaNotaFiscalCabecario {
   id: number;
   numerodanota: string;
-  data_entrada: string;
-  data_emissao_nota: string;
+  data_hora_entrada: string;
+  data_hora_emissao_nota: string;
   fornecedor = new Fornecedor();
   items_entrada: IntemsEntrada[] = [];
   arquivo_nota: string;
