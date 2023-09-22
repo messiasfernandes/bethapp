a9641988599d2c367b351807fcbb673e3adbcae8
messias fernandes <messiasfernandes@gmail.com>
2023-09-22 00:12:54 -0300

    buscar fornecedor no dialog add fonecedor ao produto

--- src/app/produto/vicular-fonecedorproduto/vicular-fonecedorproduto.component.ts
+++ src/app/produto/vicular-fonecedorproduto/vicular-fonecedorproduto.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Produtofornecedor } from './../../model/produtofornecedor';
+import { Component, OnInit } from '@angular/core';
+import { Filtro } from 'src/app/model/filtro';
+import { FornecedorService } from 'src/app/service/fornecedor.service';
 
 @Component({
   selector: 'app-vicular-fonecedorproduto',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./vicular-fonecedorproduto.component.css']
 })
 export class VicularFonecedorprodutoComponent {
+  fornecedores: any = ([] = []);
+  forncedorFiltro = new Filtro();
+  produutoforncedor= new Produtofornecedor();
+constructor(private fornecdorservice: FornecedorService){
+
+}
+
+
+  buscarForncedor(evento: any){
+    this.forncedorFiltro.pagina = 0;
+    this.forncedorFiltro.parametro = evento.query;
+    this.fornecdorservice
+      .pesquisar(this.forncedorFiltro)
+      .subscribe((dados: any) => {
+        this.fornecedores = dados.content;
+        console.log(this.fornecedores)
+       // this.totalRegistros = dados.total;
+      });
+ this.produutoforncedor.fornecedor =
+  this.fornecedores[0];
+  }
 
 }
