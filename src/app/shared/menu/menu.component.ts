28540a4be624c06a3224d83037ae52b75e36f3cf
messias fernandes <messiasfernandes@gmail.com>
2023-10-31 23:46:23 -0300

    mudança css

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -5,22 +5,20 @@ import { MenuItem, PrimeIcons } from 'primeng/api';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
-
-  exibindoMenu=false;
-  constructor( private router: Router) { }
-  menu :MenuItem[]=[]
-  mostrarMenu(){
+  exibindoMenu = false;
+  constructor(private router: Router) {}
+  menu: MenuItem[] = [];
+  mostrarMenu() {
     this.exibindoMenu = !this.exibindoMenu;
   }
-  closeSidebar(){
-
+  closeSidebar() {
     this.exibindoMenu = !this.exibindoMenu;
-    console.log(this.exibindoMenu)
+    console.log(this.exibindoMenu);
   }
-  checkActiveState(givenLink:any) {
+  checkActiveState(givenLink: any) {
     console.log(this.router.url);
     if (this.router.url.indexOf(givenLink) === -1) {
       return false;
@@ -34,107 +32,102 @@ export class MenuComponent implements OnInit {
         label: 'Cadastro',
         icon: PrimeIcons.FILE,
 
-
-            items: [
-              {
-                label: 'Clientes',
-                icon: PrimeIcons.USER_PLUS,
-              },
-              {
-                label: 'Fornecedores',
-                icon: PrimeIcons.USER_MINUS,
-
-               routerLink: ['/produtos'],
-                routerLinkActiveOptions: { exact: true },
-              },
-
-              {
-                label: 'Funicionários',
-                 icon :PrimeIcons.USER,
-                 routerLinkActiveOptions: { exact: true }
-              },
-              {
-                label: 'Produtos',
-                icon: PrimeIcons.BRIEFCASE,
-                routerLink: ['/produtos'],
-
-
-              command: () => this.closeSidebar(),
-              }
-
-            ],
+        items: [
+          {
+            label: 'Clientes',
+            icon: PrimeIcons.USER_PLUS,
 
           },
 
+          {
+            label: 'Fornecedores',
+            icon: PrimeIcons.USER_MINUS,
 
+            routerLink: ['/produtos'],
+            routerLinkActiveOptions: { exact: true },
+
+          },
+
+          {
+            label: 'Funicionários',
+            icon: PrimeIcons.USER,
+            routerLinkActiveOptions: { exact: true },
+          },
+
+          {
+            label: 'Produtos',
+            icon: ' fa-solid fa-boxes-packing fa-xl"',
+            routerLink: ['/produtos'],
+
+            command: () => this.closeSidebar(),
+          },
+        ],
+
+      },
 
       {
-        label:"Estoque",
+        label: 'Estoque',
         icon: PrimeIcons.ANGLE_DOUBLE_DOWN,
-        items:[
-         {
-            label:'Estoque Movimento',
-            icon:'fa-solid fa-warehouse fa-xl',
+        items: [
+          {
+            label: 'Estoque Movimento',
+            icon: 'fa-solid fa-warehouse fa-xl',
           },
           {
             label: 'Importar Nota fiscal',
             icon: 'fa-solid fa-file-invoice fa-xl',
             expanded: true,
-           routerLink:['/importarnotalfiscal'],
+            routerLink: ['/importarnotalfiscal'],
             command: () => this.closeSidebar(),
           },
           {
-            label:'Kits/Combos',
-            icon:PrimeIcons.TICKET,
-          }
-
-        ]
+            label: 'Kits/Combos',
+            icon: PrimeIcons.TICKET,
+          },
+        ],
       },
       {
-        label:"Financeiro",
+        label: 'Financeiro',
         icon: PrimeIcons.DOLLAR,
-        items:[
+        items: [
           {
             label: 'Condições de Pagamento',
             icon: 'pi pi-tablet',
-      //      routerLink: ['/contaspagar'],
+            //      routerLink: ['/contaspagar'],
           },
           {
             label: 'Contas Receber',
             icon: PrimeIcons.MONEY_BILL,
-           // routerLink: ['/contasreceber'],
-
+            // routerLink: ['/contasreceber'],
           },
           {
             label: 'Contas Pagar',
             icon: 'pi pi-calculator',
-      //      routerLink: ['/contaspagar'],
-          }
-
-
-        ]
-
+            //      routerLink: ['/contaspagar'],
+          },
+          {
+            label: 'Caixa',
+            icon: 'fa-solid fa-cash-register fa-xl',
+            //      routerLink: ['/contaspagar'],
+          },
+        ],
       },
       {
-        label:"Vendas",
+        label: 'Vendas',
         icon: ' fa-solid fa-cart-shopping fa-xl',
-        items:[
+        items: [
           {
             label: 'Vendas',
             icon: 'fa-solid fa-cart-plus fa-xl',
             routerLink: ['/vendas'],
-
           },
           {
             label: 'Compras',
-            icon:'pi pi-shopping-bag',
+            icon: 'pi pi-shopping-bag',
             routerLink: ['/compras'],
-          }
-
-        ]
-
-      }
+          },
+        ],
+      },
     ];
   }
-
 }
